742b1fd251ae65e552a7cf4240b4e4828ad4553a
Jun Jie Huang <1juhuang2@gmail.com>
2025-08-12 17:17:54 -0700

    style: fix eslint issues

--- src/client/__tests__/addition.test.ts
+++ src/client/__tests__/addition.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "@rbxts/jest-globals";
 
 describe("addition Tests", () => {
-    it("1 + 1 equals 2", () => {
-        expect(1 + 1).toBe(2);
-    });
+	it("1 + 1 equals 2", () => {
+		expect(1 + 1).toBe(2);
+	});
 });
